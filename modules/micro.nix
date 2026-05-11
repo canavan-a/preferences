b5f2cc30ba24efc8c02cd065d7ed5f66a0852075
canavan-a <aidan.canavan3@gmail.com>
2026-05-11 17:59:22 -0400

    added home manager and broke out micro and keyd to modules

---
+++ modules/micro.nix
@@ -0,0 +1,21 @@
+{config, pkgs, ...}:
+{
+	environment.systemPackages = with pkgs; [
+		micro
+	];
+	
+	home-manager.users.nixos = {
+		home.stateVersion = "25.11";	
+		home.file.".config/micro/settings.json".text = builtins.toJSON {
+		  colorscheme = "twilight";
+		  syntax = true;
+		  linter = true;
+		};
+
+		home.file.".config/micro/bindings.json".text = builtins.toJSON {
+		  colorscheme = "twilight";
+		  syntax = true;
+		  linter = true;
+		};
+	};
+}
