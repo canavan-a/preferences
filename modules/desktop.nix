821098b5552d84736c8ba63564521407eeaf59c8
canavan-a <aidan.canavan3@gmail.com>
2026-05-11 21:05:12 -0400

    preliminary hyprland shortcuts

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -4,7 +4,7 @@
 {
 	environment.systemPackages = with pkgs; [
 		wayvnc
-		kitty
+j		kitty
 		hyprcursor
 		adwaita-icon-theme
 		swww
@@ -34,14 +34,30 @@
 				"sleep 1 && swww img /etc/nixos/wall/w1.jpg"
 			 ];	
 			bind = [
-			    "ALT SHIFT, T, exec, kitty"
-			    "ALT SHIFT, Q, killactive,"
-			    "ALT SHIFT, B, exec, brave"
-
-				"ALT CTRL, H, movefocus, l"
-				"ALT CTRL, L, movefocus, r"
-				"ALT CTRL, K, movefocus, u"
-				"ALT CTRL, J, movefocus, d"
+				"ALT_R, T, exec, kitty"
+				"ALT_R, B, exec, brave"
+				"ALT_R SHIFT, Q, killactive,"
+				"ALT_R, H, movefocus, l"
+				"ALT_R, L, movefocus, r"
+				"ALT_R, K, movefocus, u"
+				"ALT_R, J, movefocus, d"
+				"ALT_R SHIFT, H, movewindow, l"
+				"ALT_R SHIFT, L, movewindow, r"
+				"ALT_R SHIFT, K, movewindow, u"
+				"ALT_R SHIFT, J, movewindow, d"
+				"ALT_R, A, workspace, 1"
+				"ALT_R, S, workspace, 2"
+				"ALT_R, D, workspace, 3"
+				"ALT_R, F, workspace, 4"
+				"ALT_R, G, workspace, 5"
+				"ALT_R SHIFT, A, movetoworkspace, 1"
+				"ALT_R SHIFT, S, movetoworkspace, 2"
+				"ALT_R SHIFT, D, movetoworkspace, 3"
+				"ALT_R SHIFT, F, movetoworkspace, 4"
+				"ALT_R SHIFT, G, movetoworkspace, 5"
+				"ALT_R, Space, togglefloating,"
+				"ALT_R, Return, fullscreen,"
+	
 			];
 			env = [ "XCURSOR_SIZE,12" "WLR_NO_HARDWARE_CURSORS,1" "XCURSOR_THEME,Adwaita" ];
 			input = {
