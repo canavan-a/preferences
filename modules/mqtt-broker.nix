821d6d8e824e920023726d26676b1837976dafe9
= <aidan.canvan3@gmail.com>
2026-05-25 10:53:18 -0400

    mqtt broker acl config needed

--- modules/mqtt-broker.nix
+++ modules/mqtt-broker.nix
@@ -1,11 +1,16 @@
 {config, pkgs, lib, ...}:
 {
+	environment.systemPackages = with pkgs; [
+		mosquitto	
+	];
+		
 	services.mosquitto = {
 		enable = true;
 		listeners = [{
 			port = 1883;
 			omitPasswordAuth = true;
 			settings.allow_anonymous = true;
+			acl = [ "pattern readwrite #" ];
 		}];
 	};
 	networking.firewall.allowedTCPPorts = [ 1883 ];
