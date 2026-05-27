5ed2ccad2aaa511278c8d15f56517c852ad3e2fd
canavan-a <aidan.canavan3@gmail.com>
2026-05-26 20:57:10 -0400

    updated nix

--- modules/cb-cli.nix
+++ modules/cb-cli.nix
@@ -9,6 +9,7 @@
         url = "https://github.com/ClearBlade/cb-cli/releases/download/2026.1.4/cb-cli-linux-amd64.tar.gz";
         sha256 = "ab0fb42567fa0b243aeef03c7c9aede3335b1b56afc4fcba87c6a2ec3a7b2794";
       };
+      sourceRoot = ".";
       nativeBuildInputs = [ pkgs.autoPatchelfHook ];
       installPhase = ''
         mkdir -p $out/bin
